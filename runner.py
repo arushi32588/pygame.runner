62642ad777fce273f92e6afdf5ffa0c2cceb11d4
arushi <arushiagrawal174@gmail.com>
2023-07-12 00:09:49 +0530

    Adding second regular surface to display surface: Sky

--- runner.py
+++ runner.py
@@ -8,6 +8,7 @@ pygame.display.set_caption('Runner') #Naming the title of the window
 clock = pygame.time.Clock()
 
 sky_surface = pygame.image.load('graphics/Sky.jpeg')
+ground_surface = pygame.image.load('graphics/ground.jpeg')
 
 while True:
     all_events = pygame.event.get()
@@ -16,7 +17,9 @@ while True:
             pygame.quit()
             exit() #ends the while True loop so that it cannot continue to update and throw an error
     # draw all our elements
-    screen.blit(sky_surface, (0,0)) # telling the system we want to place a surface on another
+    screen.blit(ground_surface, (0,300)) # telling the system we want to place a ground on display surface
+    screen.blit(sky_surface, (0,0)) # telling the system we want to place a sky on display surface
     # update everything
     pygame.display.update() # updates everything happening in the loop to display
     clock.tick(60) # telling the while loop it should not run more than 60 times/second
+
