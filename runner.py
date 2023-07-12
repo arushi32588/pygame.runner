85e4ef89af21f5aac8b701ce818ba7278a1c8668
arushi <arushiagrawal174@gmail.com>
2023-07-12 21:03:31 +0530

    Added player to the display surface.

--- runner.py
+++ runner.py
@@ -8,8 +8,12 @@ clock = pygame.time.Clock()
 font1 = pygame.font.Font('font/Pixeltype.ttf', 50)
 font2 = pygame.font.Font('font/Pixeltype.ttf', 25)
 
-sky_surface = pygame.image.load('graphics/Sky.jpeg')
-ground_surface = pygame.image.load('graphics/ground.jpeg')
+sky_surface = pygame.image.load('graphics/Sky.jpeg').convert()
+ground_surface = pygame.image.load('graphics/ground.jpeg').convert()
+
+player_surf = pygame.image.load('graphics/Player/walk1.png')
+player_rect = player_surf.get_rect(midbottom = (80, 300))
+
 text_surface1 = font1.render('Runner', False, 'Black')
 text_surface2 = font2.render('By Arushi', False, 'Black')
 
@@ -24,6 +28,9 @@ while True:
     screen.blit(sky_surface, (0,0)) # telling the system we want to place a sky on display surface
     screen.blit(text_surface1, (325, 50))
     screen.blit(text_surface2, (335, 80))
+    screen.blit(player_surf, player_rect)
+
+
     # update everything
     pygame.display.update() # updates everything happening in the loop to display
     clock.tick(60) # telling the while loop it should not run more than 60 times/second
