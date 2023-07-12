d63891a8d3bac0ac2893fcc73331c4db535ac618
arushi <arushiagrawal174@gmail.com>
2023-07-12 09:50:42 +0530

    Added fourth regular surface: text

--- runner.py
+++ runner.py
@@ -5,11 +5,13 @@ pygame.init() #Initiates pygame
 screen = pygame.display.set_mode((800, 400)) 
 pygame.display.set_caption('Runner') #Naming the title of the window
 clock = pygame.time.Clock()
-font = pygame.font.Font('font/Pixeltype.ttf',50)
+font1 = pygame.font.Font('font/Pixeltype.ttf', 50)
+font2 = pygame.font.Font('font/Pixeltype.ttf', 25)
 
 sky_surface = pygame.image.load('graphics/Sky.jpeg')
 ground_surface = pygame.image.load('graphics/ground.jpeg')
-text_surface = font.render('Runner', False, 'Black')
+text_surface1 = font1.render('Runner', False, 'Black')
+text_surface2 = font2.render('By Arushi', False, 'Black')
 
 while True:
     all_events = pygame.event.get()
@@ -20,7 +22,8 @@ while True:
     # draw all our elements
     screen.blit(ground_surface, (0,300)) # telling the system we want to place a ground on display surface
     screen.blit(sky_surface, (0,0)) # telling the system we want to place a sky on display surface
-    screen.blit(text_surface, (300, 50))
+    screen.blit(text_surface1, (325, 50))
+    screen.blit(text_surface2, (335, 80))
     # update everything
     pygame.display.update() # updates everything happening in the loop to display
     clock.tick(60) # telling the while loop it should not run more than 60 times/second
