9af871390b6f599c451d6d9c656eb9f262ac83ac
arushi <arushiagrawal174@gmail.com>
2023-07-13 22:55:03 +0530

    Creating exit state of game.

--- runner.py
+++ runner.py
@@ -7,6 +7,7 @@ pygame.display.set_caption('Runner') #Naming the title of the window
 clock = pygame.time.Clock()
 font1 = pygame.font.Font('font/Pixeltype.ttf', 50)
 font2 = pygame.font.Font('font/Pixeltype.ttf', 25)
+game_active = True
 
 sky_surface = pygame.image.load('graphics/Sky.jpeg').convert()
 ground_surface = pygame.image.load('graphics/ground.jpeg').convert()
@@ -31,31 +32,42 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit() #ends the while True loop so that it cannot continue to update and throw an error
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
-                player_gravity = -20
-    # draw all our elements
+        if game_active == True:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
+                    player_gravity = -23
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 700
+
+        # draw all our elements
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and player_rect.bottom == 300:
-                player_gravity = -20
-
-    screen.blit(ground_surface, (0,300)) # telling the system we want to place a ground on display surface
-    screen.blit(sky_surface, (0,0)) # telling the system we want to place a sky on display surface
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-    screen.blit(score_surf, score_rect)
-    screen.blit(by_surf, by_rect)
-    #Player
-    #Gravity
-    player_gravity+=1 
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
-    screen.blit(player_surf, player_rect)
-    screen.blit(snail_surf, snail_rect)
-    snail_rect.left -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800
+                player_gravity = -23
+    if game_active == True:
+        screen.blit(ground_surface, (0,300)) # telling the system we want to place a ground on display surface
+        screen.blit(sky_surface, (0,0)) # telling the system we want to place a sky on display surface
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        screen.blit(score_surf, score_rect)
+        screen.blit(by_surf, by_rect)
+        #Player
+        #Gravity
+        player_gravity+=1 
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+        screen.blit(snail_surf, snail_rect)
+        snail_rect.left -= 4
+        if snail_rect.right <= 0:
+            snail_rect.left = 800
+        #Collision
+        if snail_rect.colliderect(player_rect) == 1:
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
     '''keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
