7ac3bb3a367e4a01f9af6b7bb0fa55276a73c313
arushi <arushiagrawal174@gmail.com>
2023-07-14 00:00:49 +0530

    Added score bar to the game.

--- runner.py
+++ runner.py
@@ -1,6 +1,11 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks()/1000) - start_time
+    score_surf = font1.render(f'SCORE: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
 pygame.init() #Initiates pygame
 screen = pygame.display.set_mode((800, 400)) 
 pygame.display.set_caption('Runner') #Naming the title of the window
@@ -8,9 +13,10 @@ clock = pygame.time.Clock()
 font1 = pygame.font.Font('font/Pixeltype.ttf', 50)
 font2 = pygame.font.Font('font/Pixeltype.ttf', 25)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load('graphics/Sky.jpeg').convert()
-ground_surface = pygame.image.load('graphics/ground.jpeg').convert()
+ground_surface = pygame.image.load('graphics/Ground.jpeg').convert()
 
 player_surf = pygame.image.load('graphics/Player/walk1.png')
 player_rect = player_surf.get_rect(midbottom = (80, 300))
@@ -20,11 +26,11 @@ player_gravity = 0
 snail_surf = pygame.image.load('graphics/Snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (700,300))
 
-score_surf = font1.render('Runner', False, (64, 64, 64))
-score_rect = score_surf.get_rect(center = (400, 50))
+#score_surf = font1.render('Runner', False, (64, 64, 64))
+#score_rect = score_surf.get_rect(center = (400, 50))
 
-by_surf = font2.render('By Arushi', False, 'Black')
-by_rect = by_surf.get_rect(center = (400, 80))
+#by_surf = font2.render('By Arushi', False, 'Black')
+#by_rect = by_surf.get_rect(center = (400, 80))
 
 while True:
     all_events = pygame.event.get()
@@ -40,6 +46,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 700
+                start_time = int(pygame.time.get_ticks()/1000)
 
         # draw all our elements
         if event.type == pygame.KEYDOWN:
@@ -48,10 +55,11 @@ while True:
     if game_active == True:
         screen.blit(ground_surface, (0,300)) # telling the system we want to place a ground on display surface
         screen.blit(sky_surface, (0,0)) # telling the system we want to place a sky on display surface
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        screen.blit(score_surf, score_rect)
-        screen.blit(by_surf, by_rect)
+        #pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        #pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        #screen.blit(score_surf, score_rect)
+        #screen.blit(by_surf, by_rect)
+        display_score()
         #Player
         #Gravity
         player_gravity+=1 
